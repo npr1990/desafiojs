31aa1e8ac8ca99165c03d52230b11ffe835aa2a4
Nahuel Plensa Rodriguez <Nahuelpr1990@gmail.com>
2022-09-06 23:10:21 -0300

    agregue uso de array y utilice funcion de reduce

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
+// Agregado de precios.
+
+const listaValorItems = [];
+
+let consultarSiguiente = true;
+while (consultarSiguiente) {
+  const resultado = prompt("ingresa el siguiente valor (o X para salir):");
+  if (resultado == "x" || resultado == "X") {
+    consultarSiguiente = false;
+  } else listaValorItems.push(parseInt(resultado));
+}
+
 const IVA = 1.21;
 
 const descuentoEfectivo = 0.8;
 
 const descuentoDebito = 0.9;
 
-let valorProducto = parseInt(prompt("ingrese valor de producto:"));
+function sumar(a, b) {
+  return a + b;
+}
+let sumatoriaProductos = listaValorItems.reduce(sumar, 0);
+//let valorProducto = listaValorItems.reduce((a,b)=>{a+b}, 0)
 
 let modoDePago = prompt(
   "ingrese modo de pago: 1 - efectivo (20%), 2 - debito (10%), 3 - credito"
@@ -12,17 +28,19 @@ let modoDePago = prompt(
 
 function valorFinal(modo) {
   switch (modo) {
-  case "1": 
-      return valorProducto * IVA * descuentoEfectivo
+    case "1":
+      return sumatoriaProductos * IVA * descuentoEfectivo;
 
-  case "2": 
-      return valorProducto * IVA * descuentoDebito
+    case "2":
+      return sumatoriaProductos * IVA * descuentoDebito;
 
-  case "3": 
-      return valorProducto * IVA
+    case "3":
+      return sumatoriaProductos * IVA;
 
-  default: 
-      return valorProducto * IVA
+    default:
+      return sumatoriaProductos * IVA;
+  }
 }
-}
-alert("el precio final es: "+ valorFinal(modoDePago))  
+alert("el precio final es: " + valorFinal(modoDePago));
+
+console.table(listaValorItems);
