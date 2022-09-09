0e10222bcb13cc0678b3d62f181566ed985ba5ac
Nahuel Plensa Rodriguez <Nahuelpr1990@gmail.com>
2022-09-08 22:23:10 -0300

    agregue funcion para ingreso de productos al array, y filtrado de valor 0

--- js/index.js
+++ js/index.js
@@ -1,26 +1,43 @@
 // Agregado de precios.
-
-const listaValorItems = [];
-
-let consultarSiguiente = true;
-while (consultarSiguiente) {
-  const resultado = prompt("ingresa el siguiente valor (o X para salir):");
-  if (resultado == "x" || resultado == "X") {
-    consultarSiguiente = false;
-  } else listaValorItems.push(parseInt(resultado));
-}
-
-const IVA = 1.21;
+const IVA = 1.21; 
 
 const descuentoEfectivo = 0.8;
 
 const descuentoDebito = 0.9;
 
-function sumar(a, b) {
-  return a + b;
+const productos = [];
+
+let consultarSiguiente = true;
+
+function pedirSiguiente() {
+  const continuar = confirm("¿Agregar un producto?");
+  if (!continuar) {
+    consultarSiguiente = false;
+    return;
+  }
+  const nombre = prompt("ingresa el ingrese el nombre del producto:");
+  const precio = parseInt(prompt("ingresa el valor del producto, el valor '0' no se tendra en cuenta:"));
+  const producto = {nombre: nombre, precio: precio}
+  productos.push(producto);
+
+} 
+ 
+//se consulta al usuario multiples veces los valores de los productos. 
+//Los valores ingresados se añaden a Array productos
+while (consultarSiguiente) {
+  pedirSiguiente()
 }
-let sumatoriaProductos = listaValorItems.reduce(sumar, 0);
-//let valorProducto = listaValorItems.reduce((a,b)=>{a+b}, 0)
+
+//filtrado de productos con atributo precio mayor a 0
+let productosValidos = productos.filter(a=>a.precio > 0) 
+
+//funcion que suma (va dentro de reduce)
+function sumar(a, b) {
+  return a + b.precio;
+}
+//sumatoria de productos desde "0"
+let sumatoriaProductos = productosValidos.reduce(sumar, 0);
+//let valorProducto = productos.reduce((a,b)=>{a+b}, 0)
 
 let modoDePago = prompt(
   "ingrese modo de pago: 1 - efectivo (20%), 2 - debito (10%), 3 - credito"
@@ -41,6 +58,9 @@ function valorFinal(modo) {
       return sumatoriaProductos * IVA;
   }
 }
-alert("el precio final es: " + valorFinal(modoDePago));
+alert("el precio final de "+productosValidos.length+" productos es: $"+ valorFinal(modoDePago).toFixed(2));
 
-console.table(listaValorItems);
+console.table(productosValidos)
+
+//el if con array.includes(nuevoitem) filtra que no sea repetido el elemento. 
+
