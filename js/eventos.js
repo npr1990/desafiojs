b4c53d8e5e77fa92e5220a1ad3b546be45e7d4fe
Nahuel Plensa Rodriguez <Nahuelpr1990@gmail.com>
2022-09-15 20:41:47 -0300

    adapte la funcion switch al eventlistener, y agregue funcion focus y blur

---
+++ js/eventos.js
@@ -0,0 +1,77 @@
+const tabla = document.querySelector("#tablaProductos");
+const btnAgregar = document.querySelector("#btnAgregar");
+const nombreProducto = document.querySelector("#inputNombre");
+const importeProducto = document.querySelector("#inputImporte");
+const btnPrecioFinal = document.querySelector("#sumaPrecioFinal");
+const modoPagoSeleccionado = document.querySelector(
+  'input[name="modoPago"]:checked'
+  ).value;
+const parrafoPrecioFinal = document.querySelector("#verPrecioFinal");
+
+// Agregado de precios.
+const IVA = 1.21;
+
+const descuentoEfectivo = 0.8;
+
+const descuentoDebito = 0.9;
+
+const productos = [];
+
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+btnPrecioFinal.addEventListener("click", () => {
+
+
+
+  //filtrado de productos con atributo precio mayor a 0
+  let productosValidos = productos.filter((a) => a.precio > 0);
+  //funcion que suma (va dentro de reduce)
+  function sumar(a, b) {
+    return a + b.precio;
+  }
+  //sumatoria de productos desde "0"
+  let sumatoriaProductos = productosValidos.reduce(sumar, 0);
+  //let valorProducto = productos.reduce((a,b)=>{a+b}, 0)
+  let precioFinal;
+
+  switch (modoPagoSeleccionado) {
+    case "efectivo":
+      precioFinal = sumatoriaProductos * IVA * descuentoEfectivo;
+
+    case "debito":
+      precioFinal = sumatoriaProductos * IVA * descuentoDebito;
+
+    case "credito":
+      precioFinal = sumatoriaProductos * IVA;
+
+    default:
+      precioFinal = sumatoriaProductos * IVA;
+  }
+
+  parrafoPrecioFinal.innerHTML = `el precio final de ${productosValidos.length} productos es: $ ${precioFinal.toFixed(2)}`
+});
+
+btnAgregar.addEventListener("click", () => {
+  const nombre = nombreProducto.value;
+  const precio = parseInt(importeProducto.value);
+  const producto = new Producto(nombre, precio);
+  productos.push(producto);
+
+  const fila = `<tr>
+              <td>${producto.nombre}</td>
+              <td>${producto.precio}</td>
+          </tr>`;
+  tabla.innerHTML += fila;
+});
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", () => (input.className = "foco-en-input"));
+  input.addEventListener("blur", () => (input.className = ""));
+});
--- js/index.js
+++ js/index.js
@@ -1,45 +1,4 @@
-// Agregado de precios.
-const IVA = 1.21; 
-
-const descuentoEfectivo = 0.8;
-
-const descuentoDebito = 0.9;
-
-const productos = [];
-
-let consultarSiguiente = true;
-
-function pedirSiguiente() {
-  const continuar = confirm("¿Agregar un producto?");
-  if (!continuar) {
-    consultarSiguiente = false;
-    return;
-  }
-  const nombre = prompt("ingresa el ingrese el nombre del producto:");
-  const precio = parseInt(prompt("ingresa el valor del producto, el valor '0' no se tendra en cuenta:"));
-  const producto = {nombre: nombre, precio: precio}
-  productos.push(producto);
-
-} 
- 
-//se consulta al usuario multiples veces los valores de los productos. 
-//Los valores ingresados se añaden a Array productos
-while (consultarSiguiente) {
-  pedirSiguiente()
-}
-
-//filtrado de productos con atributo precio mayor a 0
-let productosValidos = productos.filter(a=>a.precio > 0) 
-
-//funcion que suma (va dentro de reduce)
-function sumar(a, b) {
-  return a + b.precio;
-}
-//sumatoria de productos desde "0"
-let sumatoriaProductos = productosValidos.reduce(sumar, 0);
-//let valorProducto = productos.reduce((a,b)=>{a+b}, 0)
-
-let modoDePago = prompt(
+/*let modoDePago = prompt(
   "ingrese modo de pago: 1 - efectivo (20%), 2 - debito (10%), 3 - credito"
 );
 
@@ -62,5 +21,5 @@ alert("el precio final de "+productosValidos.length+" productos es: $"+ valorFin
 
 console.table(productosValidos)
 
-//el if con array.includes(nuevoitem) filtra que no sea repetido el elemento. 
+//el if con array.includes(nuevoitem) filtra que no sea repetido el elemento. */
 
